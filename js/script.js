dc8629605717aba7e97cccbd8e4677cc14142f01
Benjamin Welsh <benji122889@gmail.com>
2018-03-16 11:31:13 -0400

    project meets requirements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
+// click event handler for "calculate" button
+// subtracts total product costs from retail price and presents its value in the "profit margin" input field
 document.getElementById("calcBtn").addEventListener("click", function() {
    let price = document.getElementById("price");
    let cost = document.getElementById("cost");
    let profit = price.value - cost.value;
    document.getElementById("profit").value = profit;
 });
+
+// function hatList() {
+//     for (i = 0; i < fascinators[0].length; )
+// };
---
+++ js/script1.js
@@ -0,0 +1,8 @@
+const fascinators = [
+     "img/curlycoqueleft(THG0384).jpg",
+     "img/curlycoquefront(THG0384).jpg",
+     "img/curlycoqueright(THG0384).jpg",
+     "img/curlycoqueback(THG0384).jpg"
+];
+const fascinatorList = document.querySelector('div.hatImg');
+document.getElementById("blah").src = fascinators[];
