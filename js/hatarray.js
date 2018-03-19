e1b179867637a105f6ab62ddaf80e853f70b902a
Benjamin Welsh <benji122889@gmail.com>
2018-03-18 22:55:23 -0400

    added a for loop for an array of images in hatarray.js

--- js/hatarray.js
+++ js/hatarray.js
@@ -4,5 +4,3 @@ const fascinators = [
      "img/curlycoqueright(THG0384).jpg",
      "img/curlycoqueback(THG0384).jpg"
 ];
-const fascinatorList = document.querySelector('div.hatImg');
-document.getElementById("blah").src = fascinators[];
--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+const divList = document.querySelector('div.hatImg');
+const imgList = divList.querySelectorAll('img');
+
+
+// This for loop iterates through the node list of images in the HTML document
+// while at the same time loops through the array of images in file
+// hatarray.js and applies the images to the image carousel in the document.
+
+    for (j = 0; j < imgList.length;) {
+
+        for (i = 0; i < fascinators.length; i += 1) {
+            imgList[j].src = fascinators[i];
+            if (j < imgList.length) {
+                j += 1;  
+            }
+        }
+}
+
 // click event handler for "calculate" button
 // subtracts total product costs from retail price and presents its value in the "profit margin" input field
 document.getElementById("calcBtn").addEventListener("click", function() {
-   let price = document.getElementById("price");
-   let cost = document.getElementById("cost");
-   let profit = price.value - cost.value;
-   document.getElementById("profit").value = profit;
-});
-
-// function hatList() {
-//     for (i = 0; i < fascinators[0].length; )
-// };
+    let price = document.getElementById("price");
+    let cost = document.getElementById("cost");
+    let profit = price.value - cost.value;
+    document.getElementById("profit").value = profit;
+ });
