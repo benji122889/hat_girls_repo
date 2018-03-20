13ef0a3afc5977a357afa8675a9f03856b79e8f8
Benjamin Welsh <benji122889@gmail.com>
2018-03-19 21:47:04 -0400

    added new photos for hatarray.js and started some new javascript for new list items

--- js/hatarray.js
+++ js/hatarray.js
@@ -1,6 +1,9 @@
-const fascinators = [
-     "img/curlycoqueleft(THG0384).jpg",
+const hatArray = [
+    ["img/curlycoqueleft(THG0384).jpg",
      "img/curlycoquefront(THG0384).jpg",
      "img/curlycoqueright(THG0384).jpg",
-     "img/curlycoqueback(THG0384).jpg"
+     "img/curlycoqueback(THG0384).jpg"],
+    ["img/redombrecoque1(THG0241).jpg", 
+     "img/redombrecoque2(THG0241).jpg", 
+     "img/redombrecoque3(THG0241).jpg"]
 ];
--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 const divList = document.querySelector('div.hatImg');
 const imgList = divList.querySelectorAll('img');
+const listItems = document.getElementById('List')
+
+const hatList = document.querySelector("ul.fascinators");
+const mockUp = hatList.querySelectorAll('li.mockUp');
+const cln = mockUp[0].cloneNode(true);
+
+// listItems.appendChild(cln);
 
 
-// This for loop iterates through the node list of images in the HTML document
+// This for loop iterates through the node list of image elements in the HTML document
 // while at the same time loops through the array of images in file
 // hatarray.js and applies the images to the image carousel in the document.
 
     for (j = 0; j < imgList.length;) {
 
-        for (i = 0; i < fascinators.length; i += 1) {
-            imgList[j].src = fascinators[i];
+        for (i = 0; i < hatArray[0].length; i += 1) {
+            imgList[j].src = hatArray[0][i];
             if (j < imgList.length) {
                 j += 1;  
             }
